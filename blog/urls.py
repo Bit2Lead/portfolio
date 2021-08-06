6f45ed8dca95a40188f233c5d96f284b56ddf892
Bit2Lead <bit2lead@gmail.com>
2021-08-06 21:35:29 +0600

    complete backend

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,10 @@
 from blog import views
 from django.urls import path
 
+
+app_name = "blog"
+
 urlpatterns = [
     path('', views.blog, name="blog"),
+    path('<int:blog_id>/', views.single, name="single")
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Blog
 
 # Create your views here.
 def blog(request):
 	posts = Blog.objects.all().order_by('-date')
 	return render(request, 'blog/blogs.html', {'posts':posts})
+
+def single(request, blog_id):
+	single = get_object_or_404(Blog, pk=blog_id)
+	return render(request, 'blog/single.html', {"single":single})
